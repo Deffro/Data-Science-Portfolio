66320c406b70a802cb84aa2b159332ab68870a2c
deffro <dimitris.effrosynidis@gmail.com>
2019-06-27 21:59:58 +0300

    Update functions.py

--- Notebooks/Marine-Species/functions.py
+++ Notebooks/Marine-Species/functions.py
@@ -4,6 +4,12 @@ pd.set_option('max_colwidth',50)
 pd.set_option('max_columns',250)
 pd.set_option('max_rows',500)
 
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import adfuller, kpss, acf, grangercausalitytests
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf,month_plot,quarter_plot
+from scipy import signal
+
 #visualizations
 import folium
 from folium import plugins
@@ -20,12 +26,12 @@ from plotly import tools as tls
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 from mpl_toolkits.basemap import Basemap
+from pylab import rcParams
 cf.go_offline()
 cf.set_config_file(world_readable=True,theme='white') # Make all charts public and set a global theme
 sns.set_style("whitegrid") #possible choices: white, dark, whitegrid, darkgrid, ticks
 
 ### Define function for dataset statistics ###
-
 def get_stats(df, sort_by='Different Values', sort_how=False, exclude=[], include_only=[], target='target'):
         columns = [c for c in df.columns.values if c not in exclude and c in include_only]
         df=df[columns]
@@ -117,7 +123,6 @@ def get_stats(df, sort_by='Different Values', sort_how=False, exclude=[], includ
         return mis_val_table_ren_columns
         
 ### Define function for Univariate Plots
-
 def plot_univariate(df, plot_title):
     hist_data = []
     hist_data.extend([df])
@@ -136,9 +141,8 @@ def plot_univariate(df, plot_title):
     my_fig.append_trace(trace, 1, 2)
     iplot(my_fig)
 
-### Define function for Map Plot    
-        
-def plot_map(df,variable,title):
+### Define function for Map Plot         
+def plot_map(df,variable,title,saveFig=False):
     # How much to zoom from coordinates (in degrees)
     zoom_scale = 0
     lats = df['Center Lat'].tolist()
@@ -153,7 +157,7 @@ def plot_map(df,variable,title):
     ax.set_title(title, fontsize = 20, loc='center')
     matplotlib.rcParams.update({'font.size': 14})
     # Define the projection, scale, the corners of the map, and the resolution.
-    m = Basemap(projection='cyl',llcrnrlat=bbox[0],urcrnrlat=bbox[1], llcrnrlon=bbox[2],urcrnrlon=bbox[3],lat_ts=10,resolution='h')
+    m = Basemap(projection='cyl',llcrnrlat=29.75,urcrnrlat=46.25, llcrnrlon=-6,urcrnrlon=36.5,lat_ts=10,resolution='h')
     m.bluemarble()
 
     # Draw coastlines and fill continents and water with color
@@ -161,8 +165,8 @@ def plot_map(df,variable,title):
     #m.fillcontinents(color='#CCCCCC',lake_color='lightblue')
 
     # draw parallels, meridians, and color boundaries
-    m.drawparallels(np.arange(bbox[0],bbox[1],(bbox[1]-bbox[0])/5),labels=[1,0,0,0])
-    m.drawmeridians(np.arange(bbox[2],bbox[3],(bbox[3]-bbox[2])/5),labels=[0,0,0,1],rotation=15)
+    m.drawparallels(np.arange(29.75,46.25,(46.25-30)/5),labels=[1,0,0,0])
+    m.drawmeridians(np.arange(-6,36.5,(36.5+6)/5),labels=[0,0,0,1],rotation=15)
     m.drawmapboundary(fill_color='lightblue')
 
     # format colors for elevation range
@@ -188,5 +192,84 @@ def plot_map(df,variable,title):
     cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap,norm=normalize)
 
     # save the figure and show it
-    #plt.savefig(title+'.png', format='png', dpi=500,transparent=True)
-    plt.show()            
+    if (saveFig == True):
+        plt.savefig(title+'.png', format='png', dpi=500,transparent=True)
+    plt.show()  
+
+### Define Function for Time Series Line Plot
+def time_plot(df,variable,title,y_axis_title):
+    fig, ax = plt.subplots(figsize=(15, 6))
+    sns.lineplot(df.index, df[variable] )
+
+    ax.set_title(title, fontsize = 20, loc='center', fontdict=dict(weight='bold'))
+    ax.set_xlabel('Year', fontsize = 16, fontdict=dict(weight='bold'))
+    ax.set_ylabel(y_axis_title, fontsize = 16, fontdict=dict(weight='bold'))
+    plt.tick_params(axis='y', which='major', labelsize=16)
+    plt.tick_params(axis='x', which='major', labelsize=16)
+    ax.yaxis.tick_left()  
+
+### Define Function for Time Series Seasonality Plots       
+def seasonality_plot(df,variable,title,y_axis_title=''):
+    #variable = 'salinitySurface'
+    fig, ax = plt.subplots(figsize=(15, 6))
+
+    palette = sns.color_palette("ch:2.5,-.2,dark=.3", 10)
+    sns.lineplot(df['month'], df[variable], hue=df['year'], palette=palette)
+    ax.set_title(title, fontsize = 20, loc='center', fontdict=dict(weight='bold'))
+    ax.set_xlabel('Month', fontsize = 16, fontdict=dict(weight='bold'))
+    ax.set_ylabel(y_axis_title, fontsize = 16, fontdict=dict(weight='bold'))
+
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(15, 6))
+
+    sns.boxplot(df['year'], df[variable], ax=ax[0])
+    ax[0].set_title('Year-wise Box Plot\n(The Trend)', fontsize = 20, loc='center', fontdict=dict(weight='bold'))
+    ax[0].set_xlabel('Year', fontsize = 16, fontdict=dict(weight='bold'))
+    ax[0].set_ylabel(y_axis_title, fontsize = 16, fontdict=dict(weight='bold'))
+
+    sns.boxplot(df['month'], df[variable], ax=ax[1])
+    ax[1].set_title('Month-wise Box Plot\n(The Seasonality)', fontsize = 20, loc='center', fontdict=dict(weight='bold'))
+    ax[1].set_xlabel('Month', fontsize = 16, fontdict=dict(weight='bold'))
+    ax[1].set_ylabel(y_axis_title, fontsize = 16, fontdict=dict(weight='bold'))    
+
+### Decomposition and strength of trend and seasonality###    
+def decomposition(df,variable):    
+
+    y = df[[variable]]
+    rcParams['figure.figsize'] = 15, 12
+    rcParams['axes.labelsize'] = 20
+    rcParams['ytick.labelsize'] = 16
+    rcParams['xtick.labelsize'] = 16
+    decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+    decomp = decomposition.plot()
+    decomp.suptitle('', fontsize=22)
+
+    trend_strength = max(0,(1-decomposition.resid.var()/(decomposition.resid+decomposition.trend).var())[0])
+    seasonality_strength = max(0,(1-decomposition.resid.var()/(decomposition.resid+decomposition.seasonal).var())[0])
+    print("Trend strength:",trend_strength)
+    print("Seasonality strength:",seasonality_strength)
+    
+### Augmented Dickey-Fuller Test to check for Stationarity
+def adf_test(series,title=''):
+    """
+    Pass in a time series and an optional title, returns an ADF report
+    """
+    print('Augmented Dickey-Fuller Test: {}'.format(title))
+    result = adfuller(series.dropna(),autolag='AIC') # .dropna() handles differenced data
+    
+    labels = ['ADF test statistic','p-value','# lags used','# observations']
+    out = pd.Series(result[0:4],index=labels)
+
+    for key,val in result[4].items():
+        out['critical value ({})'.format(key)]=val
+        
+    print(out.to_string())          # .to_string() removes the line "dtype: float64"
+    
+    if result[1] <= 0.05:
+        print("Strong evidence against the null hypothesis")
+        print("Reject the null hypothesis")
+        print("Data has no unit root and is stationary")
+    else:
+        print("Weak evidence against the null hypothesis")
+        print("Fail to reject the null hypothesis")
+        print("Data has a unit root and is non-stationary")
